47b2773cb4b8ade992f342f875649aaf35fe6e30
Moti Stein <moti.stein@cellebrite.com>
2025-07-18 00:32:33 +0300

    Fix analytics

--- app/main.py
+++ app/main.py
@@ -77,14 +77,13 @@ def main():
 
 # Inject Google Analytics tracking script
     components.html("""
+    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-G973RH74MN"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-G973RH74MN', {
-        'debug_mode': true
-    });
+    gtag('config', 'G-G973RH74MN');
     </script>
     """, height=0, width=0)
 
